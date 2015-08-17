cc1a96d7b8f13b7e911e3c04fcb91793a6ad2633
NickPapayiannakis <nikos.papayiannakis@gmail.com>
2015-08-17 08:18:54 -0400

    Update NameGrabber2000.php

--- NameGrabber2000.php
+++ NameGrabber2000.php
@@ -1,27 +1,4 @@
 <?php
-
-/*************************************
-*                                   *
-*                                   *
-*  Name Grabber 2000 Instructions   *
-*                                   *
-*                                   *
-*                                   *
-*************************************
-
-This mini-program scans a text file, locates every instance of a pre-determined name 
-and appends the title "Dynamite Web Designer" to it.  The location for every instance 
-of the name is logged in a file called Name Repository.txt.
-
-Instructions for use:
-
-1. Ensure that NameGrabber2000.php is in the same folder as hiddenName.txt
-2. Run NameGrabber2000.php using the method of your choice.
-3. Once finished, the program will have created two additional text files: hiddenNameModified.txt and Name Repository.txt
-
-This code makes use of file reading and writing, arrays, and string replacement.
-*/
-
 $hiddenName = file("hiddenName.txt");
 $replace = fopen ("hiddenNameModified.txt", "w");
 $nameRepository = fopen ("Name Repository.txt", "w");
